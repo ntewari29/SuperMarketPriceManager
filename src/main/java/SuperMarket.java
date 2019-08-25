7487e8097f23be58b71045640220fd737446e9d4
Nishant Tewari <nishant.tewari29@gmail.com>
2019-08-25 18:46:26 +0800

    SecondCommit

--- src/main/java/SuperMarket.java
+++ src/main/java/SuperMarket.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SuperMarket {
+    List<Inventory> inventoryList = new ArrayList<>();
+
     public List<Inventory> initializeInventory() {
-        return null;
+        return inventoryList;
+    }
+
+    public void addItem(Inventory inventory) {
+        inventoryList.add(inventory);
     }
 }
--- src/test/java/PriceManagerTest.java
+++ src/test/java/PriceManagerTest.java
@@ -1,16 +1,52 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PriceManagerTest {
+
+    SuperMarket superMarket = new SuperMarket();
+
     @Test
     public void checkTheStoreInventory() {
-        SuperMarket superMarket = new SuperMarket();
         List<Inventory> inventory = superMarket.initializeInventory();
         assertThat(inventory, is(empty()));
     }
+
+    @Test
+    public void intiateStoreInventoryAndAssertOnTheItemName() {
+        Inventory milk = new Inventory("1","Milk",5.0);
+        List<Inventory> inventory = superMarket.initializeInventory();
+        superMarket.addItem(milk);
+        assertThat(inventory, is(asList(milk)));
+    }
+
+    @Test
+    public void addItemsToInventoryAndAssertAgainstThePrice() {
+        Inventory milk = new Inventory("1","Milk",5.0);
+        Inventory corona = new Inventory("2","Corona",11.25);
+        Inventory whisky = new Inventory("3","Ardbeg",526.0);
+        superMarket.addItem(milk);
+        superMarket.addItem(corona);
+        superMarket.addItem(whisky);
+        assertThat(milk.itemPrice, is(5.0));
+    }
+
+    /*@Test
+    public void calculateTotalValueOfItemsAddedToTheInventory() {
+        Inventory milk = new Inventory("1","Milk",5.0);
+        Inventory corona = new Inventory("2","Corona",11.25);
+        Inventory whisky = new Inventory("3","Ardbeg",526.0);
+        Inventory bread = new Inventory("4","Bread",12.0);
+        superMarket.addItem(milk);
+        superMarket.addItem(corona);
+        superMarket.addItem(whisky);
+        superMarket.addItem(bread);
+        assertThat(, is(5.0));
+    }*/
 }
