af0972e5898fc6d9f866dd4d7f6fe741cffb2d47
Nishant Tewari <nishant.tewari29@gmail.com>
2019-08-25 23:48:26 +0800

    Renamed price variable

--- src/main/java/SuperMarket.java
+++ src/main/java/SuperMarket.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class SuperMarket {
     List<Inventory> inventoryList = new ArrayList<>();
     Double cartPrice = 0d;
-    Double idividualItemPrice = 0d;
+    Double individualItemPrice = 0d;
 
     public List<Inventory> initializeInventory() {
         return inventoryList;
@@ -16,9 +16,13 @@ public class SuperMarket {
 
     public Double totalValueOfCart() {
         for (int i = 0; i < inventoryList.size(); i++) {
-            idividualItemPrice = inventoryList.get(i).itemPrice;
-            cartPrice = cartPrice + idividualItemPrice;
+            individualItemPrice = inventoryList.get(i).itemPrice;
+            cartPrice = cartPrice + individualItemPrice;
         }
         return cartPrice;
+    }
+
+    public Integer itemsInTheCart() {
+        return inventoryList.size();
     }
 }
