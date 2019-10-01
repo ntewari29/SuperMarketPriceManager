9b2ef1738e9d59af6d73bda9675f39bb49097e82
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-10-01 18:06:51 +0200

    Add lombok dependency

--- src/main/java/Promotions.java
+++ src/main/java/Promotions.java
@@ -1,31 +1,13 @@
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@RequiredArgsConstructor
 class Promotions {
     private final String serialNumber;
     private final String itemOnPromo;
     private final BigDecimal promoPrice;
     private final Integer promoQty;
-
-    Promotions(String serialNumber, String itemOnPromo, BigDecimal promoPrice, Integer promoQty) {
-        this.serialNumber = serialNumber;
-        this.itemOnPromo = itemOnPromo;
-        this.promoPrice = promoPrice;
-        this.promoQty = promoQty;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public String getItemOnPromo() {
-        return itemOnPromo;
-    }
-
-    public BigDecimal getPromoPrice() {
-        return promoPrice;
-    }
-
-    public Integer getPromoQty() {
-        return promoQty;
-    }
 }
