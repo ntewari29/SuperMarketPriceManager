c4fbdecdf19b4acdb7031f03ef829da2253f38dc
Nishant Tewari <nishant.tewari29@gmail.com>
2020-04-26 20:32:28 +0800

    Corrected Jpane response text

--- src/main/java/Feedback.java
+++ src/main/java/Feedback.java
@@ -4,7 +4,7 @@ public interface Feedback {
     static void report(int qty, Inventory itemName) {
         try {
             String infoMessage = "Quantity added to cart " + qty + " for " + itemName.getItemName() + " is greater than available Store quantity " + itemName.getStoreQty();
-            JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + "Error Response", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, infoMessage, "Error Response: " + "Invalid Order Qty", JOptionPane.ERROR_MESSAGE);
         } catch (NullPointerException ne) {
             System.out.println(ne);
         }
@@ -13,7 +13,7 @@ public interface Feedback {
     static void report(Inventory inventory) {
         try {
             String infoMessage = "Invalid order attributes for item " + inventory.getItemName() + " Qty " + inventory.getStoreQty() + " Price "+ inventory.getItemPrice() + " Serial Number " + inventory.getSerialNumber();
-            JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + "Error Response", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, infoMessage, "Error Response: " + "Invalid Order Attributes", JOptionPane.ERROR_MESSAGE);
         } catch (NullPointerException ne) {
             System.out.println(ne);
         }
