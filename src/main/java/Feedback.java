c69798863a0d605dbe08b2f0204b7dbb7f0fb5d8
Nishant Tewari <nishant.tewari29@gmail.com>
2020-04-26 19:47:12 +0800

    Added logic to popup the feedback when customer adds a product with qty more than available store qty

--- src/main/java/Feedback.java
+++ src/main/java/Feedback.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+
 public interface Feedback {
     static void report(int qty, Inventory itemName) {
         try {
-            System.out.println("Quantity added to cart " + qty + " for " + itemName.getItemName() + " is greater than available Store quantity " + itemName.getStoreQty());
+            String infoMessage = "Quantity added to cart " + qty + " for " + itemName.getItemName() + " is greater than available Store quantity " + itemName.getStoreQty();
+            JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + "Error Response", JOptionPane.INFORMATION_MESSAGE);
         } catch (NullPointerException ne) {
             System.out.println(ne);
         }
